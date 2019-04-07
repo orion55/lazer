4850c5929d317b5a1838edc6ee9fb194c6f972e0
Orion55 <grol55@mail.ru>
2019-04-08 00:02:02 +0500

    сss fix #05

--- front-page.php
+++ front-page.php
@@ -17,21 +17,21 @@ get_header();
 
 global $wp_customize;
 
-if ( ! isset( $wp_customize ) ) :
+if (!isset($wp_customize)) :
 
-	$llorix_one_lite_disable_preloader = get_theme_mod( 'llorix_one_lite_disable_preloader' );
+    $llorix_one_lite_disable_preloader = get_theme_mod('llorix_one_lite_disable_preloader');
 
-	if ( isset( $llorix_one_lite_disable_preloader ) && ( $llorix_one_lite_disable_preloader != 1 ) ) :
+    if (isset($llorix_one_lite_disable_preloader) && ($llorix_one_lite_disable_preloader != 1)) :
 
-		echo '<div class="preloader">';
-		echo '<div class="status">&nbsp;</div>';
-		echo '</div>';
+        echo '<div class="preloader">';
+        echo '<div class="status">&nbsp;</div>';
+        echo '</div>';
 
-	endif;
+    endif;
 
 endif;
 
-llorix_one_lite_get_template_part( apply_filters( 'llorix_one_lite_header_layout', '/sections/llorix_one_lite_header_section' ) );
+llorix_one_lite_get_template_part(apply_filters('llorix_one_lite_header_layout', '/sections/llorix_one_lite_header_section'));
 
 ?>
 </div>
@@ -41,27 +41,27 @@ llorix_one_lite_get_template_part( apply_filters( 'llorix_one_lite_header_layout
 
 <div itemprop id="content" class="content-wrap" role="main">
 
-	<?php
+    <?php
 
-	$sections_array = apply_filters(
-		'llorix_one_companion_sections_filter',
-		array(
-			'sections/llorix_one_lite_logos_section',
-			'sections/llorix_one_lite_our_story_section',
-			'sections/llorix_one_lite_content_section',
-			'sections/llorix_one_lite_ribbon_section',
-			'sections/llorix_one_lite_latest_news_section',
-			'sections/llorix_one_lite_contact_info_section',
-			'sections/llorix_one_lite_map_section',
-		)
-	);
+    $sections_array = apply_filters(
+        'llorix_one_companion_sections_filter',
+        array(
+            'sections/llorix_one_lite_logos_section',
+            'sections/llorix_one_lite_our_story_section',
+            'sections/llorix_one_lite_content_section',
+            'sections/llorix_one_lite_ribbon_section',
+            'sections/llorix_one_lite_latest_news_section',
+            'sections/llorix_one_lite_contact_info_section',
+            'sections/llorix_one_lite_map_section',
+        )
+    );
 
-	if ( ! empty( $sections_array ) ) {
-		foreach ( $sections_array as $section ) {
-			llorix_one_lite_get_template_part( $section );
-		}
-	}
-	?>
+    if (!empty($sections_array)) {
+        foreach ($sections_array as $section) {
+            llorix_one_lite_get_template_part($section);
+        }
+    }
+    ?>
 
 </div><!-- .content-wrap -->
 
